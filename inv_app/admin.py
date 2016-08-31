3d6687e36caa629f7153889175386de6222eb762
lucas <mcbride.lucas9@gmail.com>
2016-08-30 23:14:32 -0400

    add GalleryExtended

--- inv_app/admin.py
+++ inv_app/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from inv_app.models import *
 
 
+from photologue.admin import GalleryAdmin as GalleryAdminDefault
+from photologue.models import Gallery
+
 
 admin.site.register(Inv_User)
+
+class GalleryExtendedInline(admin.StackedInline):
+    model = GalleryExtended
+    can_delete = False
+
+class GalleryAdmin(GalleryAdminDefault):
+
+    inlines = (GalleryExtendedInline,)
+
+admin.site.unregister(Gallery)
+admin.site.register(Gallery, GalleryAdmin)
--- inv_app/models.py
+++ inv_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+from photologue.models import Gallery
 
 class Inv_User(models.Model):
     user = models.OneToOneField(User)
@@ -13,3 +14,33 @@ class Inv_User(models.Model):
         return self.user.username
     def __str__(self):
         return self.user.username
+
+CATEGORIES = (
+        ('rings','rings'),
+        ('necklaces','necklaces'),
+        ('bracelets','bracelets')
+)
+
+class GalleryExtended(models.Model):
+    # Link back to Photologue's Gallery model.
+    gallery = models.OneToOneField(Gallery, related_name='extended')
+    collection = models.CharField(
+        unique=True,
+        max_length=250,
+        verbose_name='collection'
+    )
+    category = models.CharField(
+        unique=True,
+        max_length=250,
+        choices=CATEGORIES,
+        verbose_name='category'
+    )
+
+
+    # Boilerplate code to make a prettier display in the admin interface.
+    class Meta:
+        verbose_name = 'Extra fields'
+        verbose_name_plural = 'Extra fields'
+
+    def __str__(self):
+        return self.gallery.title
