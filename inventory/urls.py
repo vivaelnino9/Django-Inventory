896f539ed703bd0ab5dcbaecc13d7ad9ac6f0fdf
lucas <mcbride.lucas9@gmail.com>
2016-09-06 11:50:06 -0400

    remove grappelli

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,6 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('inv_app.urls')),
-    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
