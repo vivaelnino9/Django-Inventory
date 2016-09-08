62aa60552fbb359f2c885bd39f802ba78e0951cc
lucas <mcbride.lucas9@gmail.com>
2016-09-08 14:19:50 -0400

    change admin site name

--- inventory/urls.py
+++ inventory/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     url(r'^', include('inv_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Sherry Sheaf Administration'
